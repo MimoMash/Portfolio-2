bc95c97c7470152f0eda47cee5d801d128d0eb1c
MimoMash <mimomashyt@gmail.com>
2024-10-02 17:55:53 +0200

    Added the different language promts in the file

--- TicTacToe-main/game/language.mjs
+++ TicTacToe-main/game/language.mjs
@@ -1,10 +1,31 @@
 const DICTIONARY = {
 
     en: {
+        MENU: "MENU",
+        SETTINGS: "Settings",
+        EXIT_GAME: "Exit Game",
+        WINNER_IS: "Winner is player ",
+        GAME_OVER: "GAME OVER",
+        PLAYER_1: "one",
+        PLAYER_2: "two",
+        PLAYER: "Player ",
+        YOUR_TURN: " it is your turn",
+        PLACE_MARK: "Place your mark at: ",
         PLAY_AGAIN_QUESTION: "Play again (YES/no)? ",
         CONFIRM: "y",
+
     },
     no: {
+        MENU: "MENY",
+        SETTINGS: "Innstillinger",
+        EXIT_GAME: "Avslutt Spill",
+        WINNER_IS: "Vinneren er spiller ",
+        GAME_OVER: "SPILLET ER OVER",
+        PLAYER_1: "en",
+        PLAYER_2: "to",
+        PLAYER: "Spiller ",
+        YOUR_TURN: " det er din tur",
+        PLACE_MARK: "Plasser ditt merke : ",
         PLAY_AGAIN_QUESTION: "Spille en gang til (Ja/nei)? ",
         CONFIRM: "j",
     }
