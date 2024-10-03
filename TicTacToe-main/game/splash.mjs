0022264f2b32942f5c1fbeae71c25b7c05b9b41a
MimoMash <mimomashyt@gmail.com>
2024-10-03 03:00:41 +0200

    Added color and modified look of text couldnt make it look nice in code yet

--- TicTacToe-main/game/splash.mjs
+++ TicTacToe-main/game/splash.mjs
@@ -1,13 +1,14 @@
+import { ANSI } from "./ansi.mjs";
 
 const ART = `
- ______  ____   __      ______   ____    __      ______   ___     ___
-|      ||    | /  ]    |      | /    |  /  ]    |      | /   \\   /  _]
-|      | |  | /  /     |      ||  o  | /  /     |      ||     | /  [_
-|_|  |_| |  |/  /      |_|  |_||     |/  /      |_|  |_||  O  ||    _]
-  |  |   |  /   \\_       |  |  |  _  /   \\_       |  |  |     ||   [_
-  |  |   |  \\     |      |  |  |  |  \\     |      |  |  |     ||     |
-  |__|  |____\\____|      |__|  |__|__|\\____|      |__|   \\___/ |_____|
-
+${ANSI.COLOR.BLUE} ______  ____    _____     ${ANSI.COLOR.YELLOW}  ______   ____    _____   ${ANSI.COLOR.RED}   ______   ___   ______
+${ANSI.COLOR.BLUE}|      ||    |  /  ___|    ${ANSI.COLOR.YELLOW} |      | /    |  /  ___]  ${ANSI.COLOR.RED}  |      | /   \\ |  ____]
+${ANSI.COLOR.BLUE}|      | |  |  /  /        ${ANSI.COLOR.YELLOW} |      ||  o  | /  /      ${ANSI.COLOR.RED}  |      ||  _  || [__
+${ANSI.COLOR.BLUE}|_|  |_| |  | |  /         ${ANSI.COLOR.YELLOW} |_|  |_||     ||  /       ${ANSI.COLOR.RED}  |_|  |_|| | | ||   _]
+${ANSI.COLOR.BLUE}  |  |   |  | |  \\        ${ANSI.COLOR.YELLOW}    |  |  |  _  ||  \\    ${ANSI.COLOR.RED}       |  |  | |_| ||  [__
+${ANSI.COLOR.BLUE}  |  |   |  |  \\  \\___     ${ANSI.COLOR.YELLOW}   |  |  |  |  | \\  \\___    ${ANSI.COLOR.RED}   |  |  |     ||     |
+${ANSI.COLOR.BLUE}  |__|  |____|  \\_____|   ${ANSI.COLOR.YELLOW}    |__|  |__|__|  \\_____|   ${ANSI.COLOR.RED}   |__|   \\___/ |_____|
+${ANSI.RESET}
 `
 
 function showSplashScreen() {
